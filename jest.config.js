d40f0e205e8e87c48bb2da202ae048aee0008684
Abdul Rahman Al-Adhubi <s00191229@mail.itsligo.ie>
2024-04-28 13:29:45 +0100

    Test Lambda

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    testEnvironment: 'node',  // Ensures your tests run in a Node environment
-    testPathIgnorePatterns: ['/node_modules/'],  // Avoids running tests on node modules
-    verbose: true,  // Provides detailed information during tests
-  };
-  
